c9e5b81f60e0498f7b2e7e115c8bdda146c281ea
OlivierChiasson <olivierchiasson@hotmail.fr>
2021-04-16 17:14:36 -0300

    Started Discord Bot

--- bot/app.js
+++ bot/app.js
@@ -1,9 +1,35 @@
 // Main imports.
+const Lodash = require('lodash');
 const config = require('./config/config');
+const emoji = require('./emoji');
 const Discord = require('discord.js');
+
 
 // Init Discord class.
 const client = new Discord.Client();
+
+
+const sendEmbed = (channel, title, desc, author, date, color) => {
+    const testEmbed = new Discord.MessageEmbed()
+        .setColor('#00ff00')
+        .setTitle(title)
+        .setDescription(desc)
+        .setFooter(author + " • " + date);
+    testEmoji = "Negative Squared Latin Capital Letter A";
+        //channel.send(testEmbed);
+        channel.send({embed: testEmbed}).then(embedMessage =>{
+            embedMessage.react(emoji.letter_a);
+            embedMessage.react(emoji.letter_b);
+            embedMessage.react(emoji.letter_c);
+        
+        
+        });
+}
+
+
+
+
+
 
 // Discord events.
 client.on('ready', () => {
@@ -11,7 +37,17 @@ client.on('ready', () => {
 });
 
 client.on('message', (msg) => {
-    console.log('MSG::' + msg.content);
+    console.log('MSG:: ' + msg.content);
+
+    if(msg.content == "*abc"){
+        sendEmbed(msg.channel, "A / B / C", "Veuillez sélectionner une ou plusieurs options avec les réactions ci-dessous", "Bobby", "16/04/2021");
+    }
 });
+
+
+
+
+
+
 
 client.login(config.token);
